8565a3d50d04b48e736ccbcad36d54c9d1e0b833
Katsiy <131786428+Katsiy@users.noreply.github.com>
2025-02-18 15:06:07 +0800

    Update stackbit.config.ts

--- stackbit.config.ts
+++ stackbit.config.ts
@@ -3,14 +3,14 @@ import { defineStackbitConfig } from '@stackbit/types';
 import { GitContentSource } from '@stackbit/cms-git';
 import type { Model } from '@stackbit/types';
 
-const postModel: Model = {
-  name: 'post',
-  type: 'page',
-  urlPath: '/docs/{slug}',
+const pageModel: Model = {
+  name: 'page',
+  type: 'page', // 关键标识
+  urlPath: '/{slug}',
   fields: [
     { name: 'slug', type: 'string', required: true },
     { name: 'title', type: 'string' },
-    { name: 'content', type: 'markdown'}
+    { name: 'content', type: 'markdown' }
   ]
 };
 
@@ -35,6 +35,17 @@ export default defineStackbitConfig({
       contentDirs: ['src/content/docs'],
       branch: 'preview',
       models: [postModel] // 必须用数组格式
-    })
-  ]
+
+
+  siteMap: ({ documents }) => {
+    return documents.map(doc => {
+      if (doc.modelName === 'post') {
+        return {
+          urlPath: `/posts/${doc.fields.slug}`,
+          document: doc
+        };
+      }
+      return null;
+    }).filter(Boolean);
+  }
 });
